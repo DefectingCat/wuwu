df0d18ffe7c27a9bf5df65d14f06f0474beb37cf
xfy <xfyxfy@proton.me>
2024-07-23 09:00:53 +0800

    chore(deps): add basic dev dependencies

---
+++ src-tauri/src/init.rs
@@ -0,0 +1,5 @@
+use tauri::App;
+
+pub fn setup(app: &mut App) -> Result<(), Box<dyn std::error::Error>> {
+    Ok(())
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod init;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .setup(init::setup)
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
