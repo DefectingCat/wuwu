92998c378c89b3f742f46b666084d8124b0fbccf
DefectingCat <i@defect.ink>
2024-07-24 22:31:06 +0800

    feat(ui): add transparency for window
    remove decoration
    
    add tracing for logging

--- src-tauri/src/init.rs
+++ src-tauri/src/init.rs
@@ -1,5 +1,18 @@
 use tauri::App;
+use tracing_subscriber::{fmt, prelude::*, registry, EnvFilter};
 
-pub fn setup(app: &mut App) -> Result<(), Box<dyn std::error::Error>> {
+pub fn setup(_app: &mut App) -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
+}
+
+pub fn init_logger() {
+    let formatting_layer = fmt::layer()
+        // .pretty()
+        .with_thread_ids(false)
+        .with_target(false)
+        .with_writer(std::io::stdout);
+
+    let env_layer = EnvFilter::try_from_env("WUWU_LOG").unwrap_or_else(|_| "info".into());
+
+    registry().with(env_layer).with(formatting_layer).init();
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tracing::info;
+
 mod init;
 
 fn main() {
+    init::init_logger();
+    info!("wuwu!");
+
     tauri::Builder::default()
         .setup(init::setup)
         .run(tauri::generate_context!())
