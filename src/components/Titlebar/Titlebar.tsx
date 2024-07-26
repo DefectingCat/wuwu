d633832962a972b07715bea18e4e09a8b2d9347f
xfy <xfyxfy@proton.me>
2024-07-26 13:58:03 +0800

    feat(ui): window titlebar

--- src/components/Titlebar/Titlebar.tsx
+++ src/components/Titlebar/Titlebar.tsx
@@ -2,18 +2,38 @@ import cn from 'classnames';
 import { Component } from 'solid-js';
 import styles from './Titlebar.module.css';
 
+const titlebarButton = 'cursor-pointer w-5 h-5';
+
 const Titlebar: Component = () => {
   return (
     <>
-      <div data-tauri-drag-region class={cn(styles.titlebar, 'h-8')}>
-        <div class={styles.titlebarButton} id="titlebar-minimize">
-          <span class="icon-[solar--archive-down-minimlistic-linear]"></span>
+      <div data-tauri-drag-region class={cn(styles.titlebar)}>
+        <div id="titlebar-minimize" class={styles.titlebarButtonWrapper}>
+          <span
+            class={cn(
+              'icon-[mdi--minimize]',
+              styles.titlebarButton,
+              titlebarButton,
+            )}
+          ></span>
         </div>
-        <div class={styles.titlebarButton} id="titlebar-maximize">
-          <span class="icon-[solar--maximize-square-minimalistic-linear]"></span>
+        <div id="titlebar-maximize" class={styles.titlebarButtonWrapper}>
+          <span
+            class={cn(
+              'icon-[mdi--maximize]',
+              styles.titlebarButton,
+              'cursor-pointer w-4 h-4',
+            )}
+          ></span>
         </div>
-        <div class={styles.titlebarButton} id="titlebar-close">
-          <span class="icon-[solar--close-circle-linear]"></span>
+        <div id="titlebar-close" class={styles.titlebarButtonWrapper}>
+          <span
+            class={cn(
+              'icon-[mdi--close]',
+              styles.titlebarButton,
+              titlebarButton,
+            )}
+          ></span>
         </div>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 const { addDynamicIconSelectors } = require('@iconify/tailwind');
 
+/* https://colorhunt.co/palette/27374d526d829db2bfdde6ed */
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
